54cf1b880e0418052b018723adc79be174e26801
jcypress <fabregug@hotmail.com>
2016-01-22 00:26:43 +0100

    Commit 01
    Commit 01

--- src/Hello1.java
+++ src/Hello1.java
@@ -4,5 +4,6 @@ public class Hello1 {
 		System.out.println("My second Java Program");
 		System.out.println("I love Java");
 		System.out.println("Hello");
+		System.out.println("Github is cool");		
 	}
 }
